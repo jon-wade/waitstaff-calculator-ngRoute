81c1552ce50da7600aad9c0e7b1b712b99e5db99
jon-wade <jonwadeuk@gmail.com>
2016-04-22 15:04:35 +0100

    refine new-meal layout, including error messages and  activate cancel button

--- app/myApp.js
+++ app/myApp.js
@@ -67,47 +67,22 @@ angular.module('myApp', ['ngRoute'])
                 $rootScope.mealCount++;
                 $rootScope.averageTip = $scope.tipTotal/$scope.mealCount;
             }
+        };
 
-            //console.log($scope.mealInput);
-            //console.log($scope.mealInput.baseMealPrice.$error);
-            //console.log($scope.mealInput.taxRate.$error);
-            //console.log($scope.mealInput.tipPercentage.$error);
+        $scope.cancel = function(){
+            console.log('clearing meal amount variables');
+            $scope.err=false;
+            $scope.baseMealPrice=undefined;
+            $scope.taxRate=undefined;
+            $scope.tipPercentage=undefined;
         };
     })
     .controller('meCtrl', function($scope){
         //my-earnings page controller code in here
     });
-    //.controller('myCtrl', function($scope){
 
     //
-    //    $scope.submit = function(){
-    //        if ($scope.mealInput.$invalid){
-    //            $scope.err=true;
-    //        }
-    //        else {
-    //
-    //            $scope.subTotal = ($scope.baseMealPrice*(1+($scope.taxRate/100)));
-    //            $scope.tip = ($scope.baseMealPrice*($scope.tipPercentage/100));
-    //            $scope.customerTotal = $scope.subTotal + $scope.tip;
-    //
-    //            $scope.tipTotal += $scope.tip;
-    //            $scope.mealCount++;
-    //            $scope.averageTip = $scope.tipTotal/$scope.mealCount;
-    //        }
-    //
-    //        console.log($scope.mealInput);
-    //        console.log($scope.mealInput.baseMealPrice.$error);
-    //        console.log($scope.mealInput.taxRate.$error);
-    //        console.log($scope.mealInput.tipPercentage.$error);
-    //    };
-    //
-    //    $scope.cancel = function(){
-    //        console.log('clearing meal amount variables');
-    //        $scope.err=false;
-    //        $scope.baseMealPrice=undefined;
-    //        $scope.taxRate=undefined;
-    //        $scope.tipPercentage=undefined;
-    //    };
+
     //
     //    $scope.reset = function(){
     //        console.log('resetting all variables');
